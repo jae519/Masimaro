6d82ffa0e9542883c2b870a93172ab8f8307f5a8
Jaehyun Park <jae5199@gmail.com>
2021-03-05 21:54:31 +0900

    Modify RecyclerView related codes.

---
+++ app/src/main/java/co/kr/masimaro/home/HomeActivity.java
@@ -0,0 +1,17 @@
+package co.kr.masimaro.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import co.kr.masimaro.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R
+                .layout.activity_home);
+    }
+}
